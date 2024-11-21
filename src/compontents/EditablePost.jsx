1624fa178915fc77f66f1461636250d347aaf5c0
zxeni1 <zacxfitness@gmail.com>
2024-11-21 19:38:22 +1000

    feat: editable form fields

--- src/compontents/EditablePost.jsx
+++ src/compontents/EditablePost.jsx
@@ -3,22 +3,47 @@ import React, { Component } from 'react';
 export default class EditablePost extends Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            author: props.author,
+            location: props.location,
+            content: props.content
+        }
     }
+
+    handleAuthorChange = (event) => {
+        this.setState({
+            author: event.target.value
+        })
+    }
+
+    handleLocationChange = (event) => {
+        this.setState({
+            location: event.target.value
+        })
+    }
+
+    handleContentChange = (event) => {
+        this.setState({
+            content: event.target.value
+        })
+    }
+
 
     render(){
         return(
             <div className='EditablePost'>
                 {/* Editable author field */}
                 <label htmlFor='authorField'>Author: </label>
-                <input type='text' name='authorField' id='authorField' value={this.props.author} />
+                <input type='text' name='authorField' id='authorField' value={this.state.author} onChange={this.handleAuthorChange} />
 
                 {/* Editable location field */}
                 <label htmlFor='locationField'>Location: </label>
-                <input type='text' name='locationField' id='locationField' value={this.props.location} />
+                <input type='text' name='locationField' id='locationField' value={this.state.location} onChange={this.handleLocationChange} />
                 
                 {/* Editable content field */}
                 <label htmlFor='contentField'>Content: </label>
-                <input type='text' name='contentField' id='contentField' value={this.props.content} />
+                <input type='text' name='contentField' id='contentField' value={this.state.content} onChange={this.handleContentChange}/>
             </div>
         )
     }
